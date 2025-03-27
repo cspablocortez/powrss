a6ab727b79a8a9cce64ec3e3e15840c884394016
Pablo Cortez <57056367+cspablocortez@users.noreply.github.com>
2025-03-27 15:01:40 -0700

    View templates don't need rails-style full lang extensions

--- app.rb
+++ app.rb
@@ -6,22 +6,6 @@ require "slim"
 # urls = ["https://rss.beehiiv.com/feeds/4aF2pGVAEN.xml"]
 # urls = ["https://bucareli.blog/feed.xml", "https://desktopwords.com/feed.xml"]
 
-template = File.read("feed_template.html.erb")
-
-urls.each do |url|
-  xml = HTTParty.get(url).body
-  feed = Feedjira.parse(xml)
-
-  renderer = ERB.new(template)
-  result = renderer.result(binding)
-
-  File.open("#{feed.title.gsub(/[^\w\s]/, '')}.html", "w") do |file|
-    file.write(result)
-  end
-
-  puts "✅ Generated: #{feed.title}.html"
+get "/" do
+  slim :index
 end
-
-
-
-
